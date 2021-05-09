35a195da1cc5a30ec192ce9a6cde28b9da6ca158
Kamron Bhavnagri <kamwithk@tuta.io>
2021-05-09 17:00:35 +1000

    Minor modifications

--- src/test.py
+++ src/test.py
@@ -8,10 +8,10 @@ from stable_baselines3.common.env_checker import check_env
 from gym.wrappers.rescale_action import RescaleAction
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.ppo import PPO
+from stable_baselines3 import SAC, PPO
 
 CONTINUOUS = True
-NUM_ENVS = 5
+NUM_ENVS = 1
 
 def wrap_env(env):
     env = FlattenObservation(env)
@@ -21,6 +21,7 @@ def wrap_env(env):
 if __name__ == "__main__":
     env = make_vec_env("jd-v0", n_envs=NUM_ENVS, wrapper_class=wrap_env, env_kwargs={"jd_path": "../Game/Jelly Drift.exe", "continuous": CONTINUOUS})
 
-    model = PPO("MlpPolicy", env, verbose=1)
+    # model = PPO("MlpPolicy", env, verbose=1)
+    model = SAC("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=2500000)
     model.save("models/ppo")
