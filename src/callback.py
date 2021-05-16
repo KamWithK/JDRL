3ecaef4e9f27f0a23bbc2999c86de3774e769cfd
Kamron Bhavnagri <kamwithk@tuta.io>
2021-05-16 17:22:42 +1000

    Refactor with reduced WandB calls

--- src/callback.py
+++ src/callback.py
@@ -6,16 +6,16 @@ import plotly.graph_objects as go
 class WandBCallback(BaseCallback):
     def __init__(self, verbose: int=True, frequency=1000, ignore=["train/n_updates"]):
         super().__init__(verbose=verbose)
-
-        wandb.init(project="jelly-drift-rl", entity="lionel-polanski", name="SAC Test", dir="..", mode="online")
+        
         self.FREQUENCY = frequency
         self.IGNORE = ignore
-        self.FIRST_STEP = False
 
-    def on_first_step(self):
+        wandb.init(project="jelly-drift-rl", entity="lionel-polanski", name="SAC Test", dir="..", mode="online")
+
+    def _on_training_start(self) -> None:
         nodes = self.training_env.get_attr("BOUNDARIES")[0]
-        self.path_x = []
-        self.path_y = []
+        self.path_x, self.path_y = [], []
+
         self.fig = go.Figure()
         self.fig.add_scatter(x=nodes[:, 0, 2], y=nodes[:, 0, 0])
         self.fig.add_scatter(x=nodes[:, 1, 2], y=nodes[:, 1, 0])
@@ -27,10 +27,6 @@ class WandBCallback(BaseCallback):
         )
 
     def _on_step(self) -> bool:
-        if not self.FIRST_STEP:
-            self.FIRST_STEP = True
-            self.on_first_step()
-
         # Upload frequency control
         commit = self.num_timesteps % self.FREQUENCY
 
@@ -45,7 +41,7 @@ class WandBCallback(BaseCallback):
         self.path_y.append(position[0])
         path.x = self.path_x
         path.y = self.path_y
-        wandb.log({"position": self.fig})
+        log_dict.update({"position": self.fig})
 
         # Log dictionary
         if log_dict: wandb.log(log_dict, commit=commit)
